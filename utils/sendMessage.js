4ccdc22d87462c096edc0b508da015b8b554c963
RyannKim327 <rksesgundo123@gmail.com>
2024-11-29 15:05:41 +0800

    11-29-24 15:05 [Debugging na walang awa]

--- utils/sendMessage.js
+++ utils/sendMessage.js
@@ -1,8 +1,8 @@
 const request = require("request");
-
+const axios = require("axios");
 module.exports = async (senderID, message, pageAccessToken) => {
   console.log("Sending ...");
-  axios
+  await axios
     .post({
       url: "https://graph.facebook.com/v13.0/me/messages",
       qs: { access_token: pageAccessToken },
@@ -15,8 +15,8 @@ module.exports = async (senderID, message, pageAccessToken) => {
       console.log("Tesst");
       if (error) {
         console.error("Error sending message:", error);
-      } else if (response.body) {
-        console.error("Error response:", response.body.error);
+        // } else if (response.body) {
+        //   console.error("Error response:", response.body.error);
       } else {
         console.log("Message sent successfully:", body);
       }
@@ -25,27 +25,4 @@ module.exports = async (senderID, message, pageAccessToken) => {
       console.error(`Error [Catch]: ${e}`);
     });
   console.log("Binatong data");
-
-  // );
-  // request(
-  //   {
-  //     method: "POST",
-  //     url: "https://graph.facebook.com/v13.0/me/messages",
-  //     qs: {
-  //       access_token: pageAccessToken,
-  //     },
-  //     json: {
-  //       recipient: {
-  //         id: senderID,
-  //       },
-  //       message: message,
-  //     },
-  //   },
-  //   (error, response, body) => {
-  //     if (error) console.error(`Error [Send Message]:${error}`);
-  //     else if (response.body.error)
-  //       console.error(`Error [Response]: ${response.body.error}`);
-  //     else console.log(`Success [Send Message]: ${body}`);
-  //   },
-  // );
 };
