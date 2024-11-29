a8eddb327fdf06388843d5063fea712302908df9
RyannKim327 <rksesgundo123@gmail.com>
2024-11-29 22:44:17 +0800

    11-29-24 22:43

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const bot = require("./facebook-page/index");
+const gpt = require("./src/gpt");
 
 const api = new bot();
 api.webhookListener((event) => {
-  api.sendMessage(event.message.text, event);
+  const body = event.message.text;
+  if (body.startsWith("/")) {
+    event["message"]["text"] = body.substring(1);
+    gpt(api, event);
+  }
+  // api.sendMessage(event.message.text, event);
 });
