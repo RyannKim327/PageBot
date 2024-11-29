96b50e56850e4f6f5bbc2d47eb4d85c6c1ea0543
RyannKim327 <rksesgundo123@gmail.com>
2024-11-29 15:02:53 +0800

    11-29-24 14:59 [Debugging na walang awa]

--- utils/sendMessage.js
+++ utils/sendMessage.js
@@ -25,6 +25,7 @@ module.exports = async (senderID, message, pageAccessToken) => {
       console.error(`Error [Catch]: ${e}`);
     });
   console.log("Binatong data");
+
   // );
   // request(
   //   {
