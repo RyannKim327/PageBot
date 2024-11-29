0bf59567833f753ce6519c4afda671f44b29c623
RyannKim327 <rksesgundo123@gmail.com>
2024-11-29 12:03:25 +0800

    11-29-24 12:03

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ app.use(body.json());
 
 const VERIFY_TOKEN = "pagebot";
 const PAGE_ACCESS_TOKEN =
-  "EAAH40A5UDrYBO1LbsViUiZANJlU8DZAQye7f6OsLLZAtmQi61ivl1ZA8sthoc9ogad4I54sOZBPXEZBA8gIZCGZCa7h5eAf9GJtCwR0TVZAOr4i989uoos10Rc4ReajiObhLHGWN6eFi0XgeOrgQynZBhW25s7PXMa9184I7WQulCRr8BxVkOJAJnX7uZA9WCL7Ei12YFiVMaCn";
-
+  // "EAAH40A5UDrYBO1LbsViUiZANJlU8DZAQye7f6OsLLZAtmQi61ivl1ZA8sthoc9ogad4I54sOZBPXEZBA8gIZCGZCa7h5eAf9GJtCwR0TVZAOr4i989uoos10Rc4ReajiObhLHGWN6eFi0XgeOrgQynZBhW25s7PXMa9184I7WQulCRr8BxVkOJAJnX7uZA9WCL7Ei12YFiVMaCn";
+  "EAAH40A5UDrYBOzndZAs3UEw5hpl4l0vidoxkXwkER8YB4olPzyrSzJUVIKGbLDAyxG7IJke4gS3Bb8jsZClh0yMlXOjAZCl3Kghcbc8N6tfLP0PPLYfPDJS7yFDUNUVIwIusSeOMGB6zWZAlADPeCsFw30daL92DuhmcZBtiWvMwE6CW2lh8GSjZCfLIsr4mmWcKgJmPEx";
 app.get("/webhook", (req, res) => {
   const mode = req.query["hub.mode"];
   const token = req.query["hub.verify_token"];
