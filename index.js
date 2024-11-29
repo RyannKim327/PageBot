cc0bd4cb2dcbbde16b2dc986898fbf2cee55c5ab
RyannKim327 <rksesgundo123@gmail.com>
2024-11-29 12:44:24 +0800

    11-29-24 12:44

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ app.post("/webhook", (req, res) => {
   const body = req.body;
   if (body.object === "page") {
     body.entry.forEach((event) => {
+      console.log(event);
       if (event.message) {
         handleMessage(event, PAGE_ACCESS_TOKEN);
       } else {
