43803203fa9be7be225e81b120da89e6268aba8c
RyannKim327 <rksesgundo123@gmail.com>
2024-11-29 13:27:01 +0800

    11-29-24 13:27

--- utils/handleMessage.js
+++ utils/handleMessage.js
@@ -1,7 +1,7 @@
 const sendMessage = require("./sendMessage");
 
 module.exports = (event, pageAccessToken) => {
-  const senderID = event.recipient.id;
+  const senderID = event.sender.id;
   const messageText = event.message.text;
   sendMessage(
     senderID,
