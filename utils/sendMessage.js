134777b430da405d448f70395e7bac75921bf7e1
RyannKim327 <rksesgundo123@gmail.com>
2024-11-29 14:59:50 +0800

    11-29-24 14:59 [Debugging na walang awa]

--- utils/sendMessage.js
+++ utils/sendMessage.js
@@ -2,15 +2,16 @@ const request = require("request");
 
 module.exports = async (senderID, message, pageAccessToken) => {
   console.log("Sending ...");
-  axios.post(
-    {
+  axios
+    .post({
       url: "https://graph.facebook.com/v13.0/me/messages",
       qs: { access_token: pageAccessToken },
       body: {
         recipient: { id: senderID },
         message: message,
       },
-    }).then((error, response) => {
+    })
+    .then((error, response) => {
       console.log("Tesst");
       if (error) {
         console.error("Error sending message:", error);
@@ -19,10 +20,11 @@ module.exports = async (senderID, message, pageAccessToken) => {
       } else {
         console.log("Message sent successfully:", body);
       }
-    }).catch(e => {
-      console.error(`Error [Catch]: ${e}`)
     })
-  console.log("Binatong data"),
+    .catch((e) => {
+      console.error(`Error [Catch]: ${e}`);
+    });
+  console.log("Binatong data");
   // );
   // request(
   //   {
