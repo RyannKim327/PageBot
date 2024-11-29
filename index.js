6fe8ec009151368a072835c2386abe432bc3ccc4
RyannKim327 <rksesgundo123@gmail.com>
2024-11-29 21:04:04 +0800

    11-29-24 21:04

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ const bot = require("./facebook-page/index");
 
 const api = new bot();
 api.webhookListener((event) => {
+  console.log(`Event: ${event}`);
   api.sendMessage(event.message.text, event);
 });
