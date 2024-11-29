9d831a0f5331bcdb7a6b61e7a52ec0b5737527ed
RyannKim327 <rksesgundo123@gmail.com>
2024-11-29 13:37:50 +0800

    11-29-24 13:37 [Debugging na walang awa]

--- utils/sendMessage.js
+++ utils/sendMessage.js
@@ -4,23 +4,43 @@ module.exports = (senderID, message, pageAccessToken) => {
   console.log("Sending ...");
   request(
     {
-      method: "POST",
       url: "https://graph.facebook.com/v13.0/me/messages",
-      qs: {
-        access_token: pageAccessToken,
-      },
+      qs: { access_token: pageAccessToken },
+      method: "POST",
       json: {
-        recipient: {
-          id: senderID,
-        },
+        recipient: { id: senderID },
         message: message,
       },
     },
     (error, response, body) => {
-      if (error) console.error(`Error [Send Message]:${error}`);
-      else if (response.body.error)
-        console.error(`Error [Response]: ${response.body.error}`);
-      else console.log(`Success [Send Message]: ${body}`);
+      if (error) {
+        console.error("Error sending message:", error);
+      } else if (response.body.error) {
+        console.error("Error response:", response.body.error);
+      } else {
+        console.log("Message sent successfully:", body);
+      }
     },
   );
+  // request(
+  //   {
+  //     method: "POST",
+  //     url: "https://graph.facebook.com/v13.0/me/messages",
+  //     qs: {
+  //       access_token: pageAccessToken,
+  //     },
+  //     json: {
+  //       recipient: {
+  //         id: senderID,
+  //       },
+  //       message: message,
+  //     },
+  //   },
+  //   (error, response, body) => {
+  //     if (error) console.error(`Error [Send Message]:${error}`);
+  //     else if (response.body.error)
+  //       console.error(`Error [Response]: ${response.body.error}`);
+  //     else console.log(`Success [Send Message]: ${body}`);
+  //   },
+  // );
 };
