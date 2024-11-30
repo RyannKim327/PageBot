299b0bb1efd780fc29b08c86083835a7f6d68e30
RyannKim327 <rksesgundo123@gmail.com>
2024-11-30 11:21:03 +0800

    11-30-24 11:20

--- src/cleargpt.js
+++ src/cleargpt.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 module.exports = (api, event) => {
   const data = JSON.parse(fs.readFileSync("data/gpt.json", "utf-8"));
   data[event.sender.id] = [];
-  fs.writeFileSync("data/gpt.json", JSON.sstringify(data), "utf-8");
+  fs.writeFileSync("data/gpt.json", JSON.stringify(data), "utf-8");
   api.sendMessage(
     {
       text: "The past queries are now cleared",
