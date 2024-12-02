7126df2c7b46109df0f300f5d91f9cc730417e64
RyannKim327 <rksesgundo123@gmail.com>
2024-12-02 21:54:23 +0800

    12-02-24 21:54

--- src/imagine.js
+++ src/imagine.js
@@ -1,5 +1,6 @@
 module.exports = (api, event, regex) => {
   const body = event.message.text.match(regex);
+  api.sendMessage("Generating... ", event);
   api.sendAttachment(
     "image",
     `https://image.pollinations.ai/prompt/${encodeURIComponent(body[1])}`,
