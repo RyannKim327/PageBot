bf9af2d104e323702aee90e49b5e39a46038b747
RyannKim327 <rksesgundo123@gmail.com>
2024-12-10 21:44:22 +0800

    12-10-24 21:44

---
+++ src/bible.js
@@ -0,0 +1,10 @@
+const biblegateway = require("biblegateway-scrape");
+
+module.exports = async (api, event, regex) => {
+  const data = event.message.text.match(regex)[1];
+  const bible = await biblegateway.verse(
+    data,
+    biblegateway.version.ENG_NEW_LIVING_TRANSLATION,
+  )[0];
+  api.sendMessage(`${bible.book}\n\n${bible.verse}`, (failed, response) => {});
+};
