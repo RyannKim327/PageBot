0480e1d0a9adf773995cd0d3ee1db643515fc4f6
RyannKim327 <rksesgundo123@gmail.com>
2025-01-23 06:07:19 +0800

    01-23-25 06:07

--- src/guitar.js
+++ src/guitar.js
@@ -1,12 +1,14 @@
-const UltimateGuitar = require("ultimate-guitar")
+const UltimateGuitar = require("ultimate-guitar");
 
 module.exports = async (api, event, regex) => {
-  const guitar = new UltimateGuitar()
-  const body = event.message.text.match(regex)
-  await guitar.init(body[1])
-  const result = await guitar.fetch_data(UltimateGuitar.RANDOM)
+  const body = event.message.text.match(regex);
+  let data = await UltimateGuitar.searchSong(body[1]);
+  const result = await UltimateGuitar.fetchChords(data);
 
-  const chords = await UltimateGuitar.get_chords(result.url)
+  const chords = result.responses;
 
-  api.sendMessage(`Song Name: ${chords.song_name}\nArtist: ${chords.artist_name}\n\n${chords.chords}`, event)
-}
+  api.sendMessage(
+    `Song Name: ${chords.song_name}\nArtist: ${chords.artist_name}\n\n${chords.chords}`,
+    event,
+  );
+};
