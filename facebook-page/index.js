b94834f555671da26a84a500691add1ae9bc86f0
RyannKim327 <rksesgundo123@gmail.com>
2025-08-20 02:30:20 +0800

    08-20-25  2:30

--- facebook-page/index.js
+++ facebook-page/index.js
@@ -19,7 +19,7 @@ class FacebookPage {
     this.prefix = "/";
     this.commands = [];
     this.start = true;
-    this.version = "v22.0";
+    this.version = "v23.0";
     this.fallback = null;
     this.types = {
       audio: "audio/mpeg",
