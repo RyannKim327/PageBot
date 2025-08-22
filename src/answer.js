16be6312dbd482300de4e93c624639426fa7c85a
RyannKim327 <rksesgundo123@gmail.com>
2025-08-23 00:23:42 +0800

    08-23-25  0:23

--- src/answer.js
+++ src/answer.js
@@ -3,5 +3,13 @@ const axios = require("axios");
 module.exports = async (api, event, regex) => {
   const body = event.message.text.match(regex)[1];
   // const { data } = await axios.get();
-  api.sendMessage("Test Reply", event, (failed, response) => {});
+  if (body === "This_is_a_challenge_for_all_oudjsa") {
+    api.sendMessage(
+      "Congratiolations, you've got it.",
+      event,
+      (failed, response) => {},
+    );
+  } else {
+    api.sendMessage("Sorry, wrong flag", event, (failed, response) => {});
+  }
 };
