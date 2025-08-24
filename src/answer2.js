d7c3d7f1c66a257d9866af8a1828cce0773c5756
RyannKim327 <rksesgundo123@gmail.com>
2025-08-24 12:46:36 +0800

    08-24-25 12:46 - [Fixing challenge 2]

--- src/answer2.js
+++ src/answer2.js
@@ -1,9 +1,9 @@
 module.exports = async (api, event, regex) => {
-	const code = "aselfencryptioncreatedforallnewbieslikeme"
-	const body = event.message.text(regex)[1].toLowerCase()
-	if (code === body) {
-		api.sendMessage("You've got it, congratiolations", event)
-	} else {
-		api.sendMessage("Wrong flag, please try again", event)
-	}
-}
+  const code = "aselfencryptioncreatedforallnewbieslikeme";
+  const body = event.message.text.match(regex)[1].toLowerCase();
+  if (code === body) {
+    api.sendMessage("You've got it, congratiolations", event);
+  } else {
+    api.sendMessage("Wrong flag, please try again", event);
+  }
+};
