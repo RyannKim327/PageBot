864d6dd360b655ad120be2f7771eb5fcdd0a0457
RyannKim327 <rksesgundo123@gmail.com>
2025-08-24 09:58:40 +0800

    08-24-25  9:58

--- src/qr.js
+++ src/qr.js
@@ -2,13 +2,10 @@ const axios = require("axios");
 
 module.exports = async (api, event, regex) => {
   const body = event.message.text.match(regex);
-  api.sendMessage("Generated QR Code", event, (failed, response) => {
-    if (!failed) {
-      api.sendAttachment(
-        "image",
-        `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${body[1]}`,
-        (failed, response) => {},
-      );
-    }
-  });
+  api.sendMessage("Generated QR Code", event, (failed, response) => {});
+  api.sendAttachment(
+    "image",
+    `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${body[1]}`,
+    (failed, response) => {},
+  );
 };
