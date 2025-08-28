6ed14f4400be536a54b75257d83b92a08941ec9f
RyannKim327 <rksesgundo123@gmail.com>
2025-08-29 06:58:12 +0800

    08-29-25  6:58

--- src/chill.js
+++ src/chill.js
@@ -1,7 +1,8 @@
 const md = require("./../utils/markdown");
 module.exports = (api, event) => {
   api.sendMessage(md("Chill ka lang"), event);
-  console.log(JSON.stringify(event, null, 2))
+  console.log(JSON.stringify(event, null, 2));
+  api.sendToAdmin("Hello Admin");
   api.sendAttachment(
     "image",
     `${__dirname}/../assets/chill.png`,
