1d0fa321781ded64067b402f5ccd7373ac7cf7a3
RyannKim327 <rksesgundo123@gmail.com>
2025-09-11 05:29:47 +0800

    09-11-25 05:29 - [Fixed minor problems]

--- src/account.js
+++ src/account.js
@@ -2,7 +2,7 @@ const { get, post } = require("../utils/gist");
 
 module.exports = async (api, event, regex) => {
 	const accounts = await get("accounts.json");
-	const name = event.message.text(regex)[1];
+	const name = event.message.text.match(regex)[1];
 
 	if (accounts[event.sender.id]) {
 		return api.sendMessage("You're already registered", event);
