fb956e7a621ea430c08caf489d70a9e526d1640e
RyannKim327 <rksesgundo123@gmail.com>
2025-11-07 06:49:13 +0800

    11-07-25  6:49

--- src/cleargpt.js
+++ src/cleargpt.js
@@ -1,18 +1,19 @@
 const fs = require("fs");
 
 module.exports = (api, event) => {
-  const data = JSON.parse(fs.readFileSync("data/gpt.json", "utf-8"));
-  data[event.sender.id] = [
-    {
-      role: "user",
-      content:
-        "Pretend to be the girlfriend of Ryann Kim Sesgundo named Nix. Ask my name first before we start any communication",
-    },
-    {
-      role: "system",
-      content: "Hello, my name is Nix, may I ask your name?",
-    },
-  ];
-  fs.writeFileSync("data/gpt.json", JSON.stringify(data), "utf-8");
+  // const data = JSON.parse(fs.readFileSync("data/gpt.json", "utf-8"));
+  // data[event.sender.id] = [
+  //   {
+  //     role: "user",
+  //     content:
+  //       "Pretend to be the girlfriend of Ryann Kim Sesgundo named Nix. Ask my name first before we start any communication",
+  //   },
+  //   {
+  //     role: "system",
+  //     content: "Hello, my name is Nix, may I ask your name?",
+  //   },
+  // ];
+  // fs.writeFileSync("data/gpt.json", JSON.stringify(data), "utf-8");
+  const { data } = await axios, get(`https://pollinations-ai-sigma.vercel.app/delete/?user=${encodeURIComponent(event.message.text.substring(prefix.length))}`)
   api.sendMessage("The past queries are now cleared", event);
 };
