66725423cf824b82591d1c5a0684f261f3ea2327
RyannKim327 <rksesgundo123@gmail.com>
2026-01-26 22:00:33 +0800

    01-26-26 22:00 [I think this will work now]

--- src/guitar.js
+++ src/guitar.js
@@ -2,13 +2,17 @@ const UltimateGuitar = require("ultimate-guitar");
 
 module.exports = async (api, event, regex) => {
   const body = event.message.text.match(regex);
-  let data = await UltimateGuitar.searchSong(body[1], UltimateGuitar.CHORDS);
-  const result = await UltimateGuitar.fetchChords(data.responses[0]);
+  const data = await UltimateGuitar.searchSong(
+    body[1],
+    UltimateGuitar.category.CHORDS,
+  );
+  const response = data.responses[0];
+  const result = await UltimateGuitar.fetchChords(data);
 
   const chords = result.response;
 
   api.sendMessage(
-    `Song Name: ${data.song_name}\nArtist: ${data.artist_name}\n\n${chords.chords}`,
+    `Song Name: ${response.song_name}\nArtist: ${response.artist_name}\n\n${chords}`,
     event,
   );
 };
