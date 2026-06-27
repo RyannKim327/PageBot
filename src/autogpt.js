993ac29036247487a9c1be7e702d7aff4d87316b
RyannKim327 <rksesgundo123@gmail.com>
2026-06-27 19:21:45 +0800

    06-27-26 19:21

--- src/autogpt.js
+++ src/autogpt.js
@@ -1,13 +1,18 @@
+import md from "./../utils/markdown.js";
+import { fileURLToPath } from "url";
+import path from "path";
 import fs from "fs"
 
-export default async (api, event) => {
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export default (api, event) => {
   const data = JSON.parse(fs.readFileSync(`data/auto_user.json`, "utf-8"))
   if (data[event.sender.id]) {
     data[event.sender.id] = true
-    api.sendMessage("Auto AI True")
+    api.sendMessage("Auto AI True", event)
   } else {
     data[event.sender.id] = undefined
-    api.sendMessage("Auto AI False")
+    api.sendMessage("Auto AI False", event)
   }
   fs.writeFileSync(`data/auto_user.json`, JSON.stringify(data, null, 2), 'utf-8')
-}
+};
