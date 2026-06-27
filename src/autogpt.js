a1f8b5a29af5c8f4d7c308ed329eb7280e4a3826
RyannKim327 <rksesgundo123@gmail.com>
2026-06-27 19:26:21 +0800

    06-27-26 19:26

--- src/autogpt.js
+++ src/autogpt.js
@@ -6,13 +6,13 @@ import fs from "fs"
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default (api, event) => {
-  const data = JSON.parse(fs.readFileSync(`data/auto_user.json`, "utf-8"))
-  if (data[event.sender.id]) {
+  const data = JSON.parse(fs.readFileSync(`${__dirname}/data/auto_user.json`, "utf-8"))
+  if (data[event.sender.id] !== undefined) {
     data[event.sender.id] = undefined
     api.sendMessage("Auto AI False", event)
   } else {
     data[event.sender.id] = true
     api.sendMessage("Auto AI True", event)
   }
-  fs.writeFileSync(`data/auto_user.json`, JSON.stringify(data, null, 2), 'utf-8')
+  fs.writeFileSync(`${__dirname}/data/auto_user.json`, JSON.stringify(data, null, 2), 'utf-8')
 };
